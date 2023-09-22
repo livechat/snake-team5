3e8c0a3f96c355fde5c37c68798575a24825a553
Jakub Sikora <j.sikora@livechatinc.com>
2023-09-22 11:49:25 +0200

    changes

--- src/components/GameBoard.tsx
+++ src/components/GameBoard.tsx
@@ -1,13 +1,15 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import styled from '@emotion/styled'
 import { useState, useEffect } from 'react'
 import InitialStageInput from './InitialStageInput'
 import GameOverInput from './GameOverInput'
+import ScoreBoard from './ScoreBoard'
 
-const GRID_SIZE = 20
-const INITIAL_SNAKE_POSITION = [
-	{ x: GRID_SIZE / 2, y: GRID_SIZE / 2 },
-	{ x: GRID_SIZE / 2 + 1, y: GRID_SIZE / 2 },
-]
+// const GRID_SIZE = 20
+// const INITIAL_SNAKE_POSITION = [
+// 	{ x: GRID_SIZE / 2, y: GRID_SIZE / 2 },
+// 	{ x: GRID_SIZE / 2 + 1, y: GRID_SIZE / 2 },
+// ]
 
 const GAME_STAGES = {
 	INITIAL: 'initial',
@@ -16,51 +18,25 @@ const GAME_STAGES = {
 }
 
 const Wrapper = styled.div`
+	position: relative;
 	width: 100%;
 	height: 100%;
 `
 
-const StyledHeader = styled.header`
-	height: 70px;
+const IframeGame = styled.div`
 	width: 100%;
-	background-color: rebeccapurple;
-`
-
-const Board = styled.div`
-	height: calc(100% - 70px);
-	width: 100%;
-	background-color: #ccc;
-	display: grid;
-	grid-template-columns: repeat(20, 1fr);
-	grid-template-rows: repeat(20, 1fr);
-`
-
-const Cell = styled.div`
 	height: 100%;
-	width: 100%;
-	background-color: #ccc;
-	border: 1px solid;
 `
 
-const Food = styled.div`
-	height: 100%;
+const IFrame = styled.iframe`
 	width: 100%;
-	background-color: red;
-	border: 1px solid;
+	height: 100%;
+	border: none;
 `
 
-const Snake = styled.div`
-	height: 100%;
-	width: 100%;
-	background-color: green;
-	border: 1px solid;
-`
-
-const SnakeHead = styled.div`
-	height: 100%;
-	width: 100%;
-	background-color: black;
-	border: 1px solid;
+const ScoreBoardWrapper = styled.div`
+	position: relative;
+	top: -87px;
 `
 
 const GameBoard = () => {
@@ -68,90 +44,87 @@ const GameBoard = () => {
 
 	// @ts-ignore - to make build pass for now :)
 	const [words, setWords] = useState<string[]>([])
-	const [selectedWord, setSelectedWord] = useState<string>('')
-	const [snakePosition, setSnakePosition] = useState(INITIAL_SNAKE_POSITION)
-	const [foodPosition, setFoodPosition] = useState({ x: 5, y: 5 })
 	const [direction, setDirection] = useState('left')
-	const [score, setScore] = useState(0)
+	const [score] = useState(0)
 
-	const renderBoard = () => {
-		let cells = []
-		for (let row = 0; row < GRID_SIZE; row++) {
-			for (let col = 0; col < GRID_SIZE; col++) {
-				const isFood = foodPosition.x === col && foodPosition.y === row
-				const isSnake = snakePosition.find((snakeCell) => snakeCell.x === row && snakeCell.y === col)
+	// const renderBoard = () => {
+	// 	let cells = []
+	// 	for (let row = 0; row < GRID_SIZE; row++) {
+	// 		for (let col = 0; col < GRID_SIZE; col++) {
+	// 			const isFood = foodPosition.x === col && foodPosition.y === row
+	// 			const isSnake = snakePosition.find((snakeCell) => snakeCell.x === row && snakeCell.y === col)
 
-				const isSnakeHead = snakePosition[0].x === row && snakePosition[0].y === col
+	// 			const isSnakeHead = snakePosition[0].x === row && snakePosition[0].y === col
 
-				if (isSnakeHead) {
-					const snakeHead = <SnakeHead key={`${row}-${col}`} />
-					cells.push(snakeHead)
-					continue
-				}
+	// 			if (isSnakeHead) {
+	// 				const snakeHead = <SnakeHead key={`${row}-${col}`} />
+	// 				cells.push(snakeHead)
+	// 				continue
+	// 			}
 
-				if (isSnake) {
-					const snake = <Snake key={`${row}-${col}`} />
-					cells.push(snake)
-					continue
-				}
+	// 			if (isSnake) {
+	// 				const snake = <Snake key={`${row}-${col}`} />
+	// 				cells.push(snake)
+	// 				continue
+	// 			}
 
-				if (isFood) {
-					const food = <Food key={`${row}-${col}`} />
-					cells.push(food)
-					continue
-				}
+	// 			if (isFood) {
+	// 				const food = <Food key={`${row}-${col}`} />
+	// 				cells.push(food)
+	// 				continue
+	// 			}
 
-				const cell = <Cell key={`${row}-${col}`} />
-				cells.push(cell)
-			}
-		}
-		return cells
-	}
+	// 			const cell = <Cell key={`${row}-${col}`} />
+	// 			cells.push(cell)
+	// 		}
+	// 	}
+	// 	return cells
+	// }
 
-	const gameOver = () => {
-		console.log('Game Over')
-		setGameStage(GAME_STAGES.OVER)
-	}
+	// const gameOver = () => {
+	// 	console.log('Game Over')
+	// 	setGameStage(GAME_STAGES.OVER)
+	// }
 
-	const updateGame = () => {
-		let newSnakePosition = [...snakePosition]
+	// const updateGame = () => {
+	// 	let newSnakePosition = [...snakePosition]
 
-		if (
-			snakePosition[0].x < 0 ||
-			snakePosition[0].x >= GRID_SIZE ||
-			snakePosition[0].y < 0 ||
-			snakePosition[0].y >= GRID_SIZE
-		) {
-			gameOver()
-			return
-		}
+	// 	if (
+	// 		snakePosition[0].x < 0 ||
+	// 		snakePosition[0].x >= GRID_SIZE ||
+	// 		snakePosition[0].y < 0 ||
+	// 		snakePosition[0].y >= GRID_SIZE
+	// 	) {
+	// 		gameOver()
+	// 		return
+	// 	}
 
-		switch (direction) {
-			case 'left':
-				newSnakePosition.unshift({ x: newSnakePosition[0].x, y: newSnakePosition[0].y - 1 })
-				break
-			case 'right':
-				newSnakePosition.unshift({ x: newSnakePosition[0].x, y: newSnakePosition[0].y + 1 })
-				break
-			case 'up':
-				newSnakePosition.unshift({ x: newSnakePosition[0].x - 1, y: newSnakePosition[0].y })
-				break
-			case 'down':
-				newSnakePosition.unshift({ x: newSnakePosition[0].x + 1, y: newSnakePosition[0].y })
-				break
-		}
+	// 	switch (direction) {
+	// 		case 'left':
+	// 			newSnakePosition.unshift({ x: newSnakePosition[0].x, y: newSnakePosition[0].y - 1 })
+	// 			break
+	// 		case 'right':
+	// 			newSnakePosition.unshift({ x: newSnakePosition[0].x, y: newSnakePosition[0].y + 1 })
+	// 			break
+	// 		case 'up':
+	// 			newSnakePosition.unshift({ x: newSnakePosition[0].x - 1, y: newSnakePosition[0].y })
+	// 			break
+	// 		case 'down':
+	// 			newSnakePosition.unshift({ x: newSnakePosition[0].x + 1, y: newSnakePosition[0].y })
+	// 			break
+	// 	}
 
-		const isFoodEaten = newSnakePosition[0].x === foodPosition.x && newSnakePosition[0].y === foodPosition.y
+	// 	const isFoodEaten = newSnakePosition[0].x === foodPosition.x && newSnakePosition[0].y === foodPosition.y
 
-		if (isFoodEaten) {
-			foodRandomizer()
-			setScore((prevScore) => prevScore + 1)
-		} else {
-			newSnakePosition.pop()
-		}
+	// 	if (isFoodEaten) {
+	// 		foodRandomizer()
+	// 		setScore((prevScore) => prevScore + 1)
+	// 	} else {
+	// 		newSnakePosition.pop()
+	// 	}
 
-		setSnakePosition(newSnakePosition)
-	}
+	// 	setSnakePosition(newSnakePosition)
+	// }
 
 	const updateDirection = (event: any) => {
 		switch (event.key) {
@@ -170,11 +143,11 @@ const GameBoard = () => {
 		}
 	}
 
-	const foodRandomizer = () => {
-		const x = Math.floor(Math.random() * GRID_SIZE)
-		const y = Math.floor(Math.random() * GRID_SIZE)
-		setFoodPosition({ x, y })
-	}
+	// const foodRandomizer = () => {
+	// 	const x = Math.floor(Math.random() * GRID_SIZE)
+	// 	const y = Math.floor(Math.random() * GRID_SIZE)
+	// 	setFoodPosition({ x, y })
+	// }
 
 	const fetchWords = async (category: string) => {
 		console.log('category', category)
@@ -197,7 +170,7 @@ const GameBoard = () => {
 			const words = ['hello', 'world']
 
 			setWords(words)
-			setSelectedWord(words[0])
+			// setSelectedWord(words[0])
 
 			setGameStage(GAME_STAGES.PLAYING)
 		} catch (error) {
@@ -208,7 +181,7 @@ const GameBoard = () => {
 	useEffect(() => {
 		const interval = setInterval(() => {
 			if (gameStage !== GAME_STAGES.PLAYING) return
-			updateGame()
+			// updateGame()
 		}, 500)
 		return () => clearInterval(interval)
 	})
@@ -236,10 +209,13 @@ const GameBoard = () => {
 
 	return (
 		<Wrapper>
-			<StyledHeader>
-				Word: {selectedWord}, score: {score}
-			</StyledHeader>
-			<Board>{renderBoard()}</Board>
+			<IframeGame>
+				<IFrame src="https://snake-vue.netlify.app/" />
+			</IframeGame>
+
+			<ScoreBoardWrapper>
+				<ScoreBoard text="Start!" />
+			</ScoreBoardWrapper>
 		</Wrapper>
 	)
 }
